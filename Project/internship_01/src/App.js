d026c12edec523a8b2f3faca16debde5a7683f2d
Aashish Thakur <j9183208@gmail.com>
2023-05-29 14:03:16 +0530

    Header File

---
+++ Project/internship_01/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div class="text-3xl font-bold underline text-clifford text-[#50d71e]">
+     <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Project/internship_01/src/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <div>
+            <nav>
+                <h3>LOgo</h3>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li> Contact</li>
+                </ul>
+                <button>Login</button>
+            </nav>
+        </div>
+    </div>
+  )
+}
+export default Header
