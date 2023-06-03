45bf1c98b356eeb971446fd9b096f191fc087007
Aashish Thakur <j9183208@gmail.com>
2023-06-03 22:50:36 +0530

    Footer

---
+++ Project/internship_01/src/Header/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+            <h2>Fotter part</h2>
+        </footer>
+
+    </div>
+  )
+}
+
+export default Footer
