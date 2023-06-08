72586dbdff5eaff7066dfb2fba8abdf80cfcd1e1
Aashish Thakur <j9183208@gmail.com>
2023-06-08 10:45:42 +0530

    Categories Added

--- Project/internship_01/src/Categories/Button.js
+++ Project/internship_01/src/Categories/Button.js
@@ -1,33 +1,54 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import Conatiner from '../Container/Conatiner'
 
 const Button = () => {
-  const[userInput, setuserInput] =useState();
-  useEffect(() => {
-    const getData=async()=>
-    {
-      const {data}=await axios.get(`https://api.themoviedb.org/3/search/movie?query=${setuserInput}?&api_key=db9c8f6101242bb4f50b946dd0fc7571`)
-      // console.log("data",data.results);
-      setuserInput(data.results)
-    }
-    getData()
-  }, []);
+  const [Data, Setdata] = useState([]);
 
-  
+  const handleClick = async (value) => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+          value
+        )}&api_key=db9c8f6101242bb4f50b946dd0fc7571`
+      );
+      Setdata(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='w-[100%]'>
-        <div className='flex  justify-center gap-[30px] border-red-300 border-8 '>
-        <button  className='p-[5px] border-4 border-green-300'>Action</button>
-        <button className='p-[5px] border-4 border-green-300'>Comedy</button>
-        <button className='p-[5px] border-4 border-green-300'>Adventure</button>
-        <button className='p-[5px] border-4 border-green-300'>Horror</button>
-        <button className='p-[5px] border-4 border-green-300'>Drama</button>
-        
+      <div className='flex justify-center gap-[30px] border-red-300 border-8 '>
+        <button onClick={() => handleClick('Action')} className='p-[5px] border-4 border-green-300'>
+          Action
+        </button>
+        <button onClick={() => handleClick('Comedy')} className='p-[5px] border-4 border-green-300'>
+          Comedy
+        </button>
+        <button onClick={() => handleClick('Adventure')} className='p-[5px] border-4 border-green-300'>
+          Adventure
+        </button>
+        <button onClick={() => handleClick('Horror')} className='p-[5px] border-4 border-green-300'>
+          Horror
+        </button>
+        <button onClick={() => handleClick('Drama')} className='p-[5px] border-4 border-green-300'>
+          Drama
+        </button>
+      </div>
+      {Data?.map((res) => (
+        <div key={res.id} className='w-[300px] border-black border-8 p-[2px]'>
+          <img
+            className=''
+            src={`https://image.tmdb.org/t/p/w500${res.poster_path}`}
+            alt='bg img'
+          />
+          <h2>{res.original_title}</h2>
+          <h3>Ratings {res.vote_average} ⭐</h3>
         </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- Project/internship_01/src/Container/Conatiner.js
+++ Project/internship_01/src/Container/Conatiner.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Button from "../Categories/Button";
+// import Button from "../Categories/Button";
 
 
 
 const Conatiner = () => {
   const [Data, Setdata] = useState([]);
+
+  const handleClick = async (value) => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+          value
+        )}&api_key=db9c8f6101242bb4f50b946dd0fc7571`
+      );
+      Setdata(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     const getData=async()=>
     {
@@ -17,7 +31,24 @@ const Conatiner = () => {
   }, []);
   return (
     <div className="w-full border-green-800 border-8 flex flex-col justify-center items-center">
-     <Button/>
+     <div className='flex justify-center gap-[30px] border-red-300 border-8 '>
+        <button onClick={() => handleClick('Action')} className='p-[5px] border-4 border-green-300'>
+          Action
+        </button>
+        <button onClick={() => handleClick('Comedy')} className='p-[5px] border-4 border-green-300'>
+          Comedy
+        </button>
+        <button onClick={() => handleClick('Adventure')} className='p-[5px] border-4 border-green-300'>
+          Adventure
+        </button>
+        <button onClick={() => handleClick('Horror')} className='p-[5px] border-4 border-green-300'>
+          Horror
+        </button>
+        <button onClick={() => handleClick('Drama')} className='p-[5px] border-4 border-green-300'>
+          Drama
+        </button>
+        
+      </div>
       <div className="containerw-full mt-[10px] flex flex-wrap  justify-around gap-[30px] border-4  ">
        
          
@@ -35,6 +66,7 @@ const Conatiner = () => {
             />
             <h2>{res.original_title}</h2>
             <h3>Ratings {res.vote_average} ⭐</h3>
+         
           </div>
 
         ))}
