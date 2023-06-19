0f16d133c922b072328242cd1a76086483bf8560
Aashish Thakur <j9183208@gmail.com>
2023-06-19 22:45:16 +0530

    Search bar updated

---
+++ Project/internship_01/src/Shimmer/InfoShimmer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Shimmer/crusal.css"
+const InfoShimmer =()=>{
+    return(
+        <div>
+            <div className="flex justify-center items-center flex-col gap-[50px] bg-black">
+            <div className="image-container">
+
+            </div>
+            <div className="para">
+
+            </div>
+
+            <div className="image-container2">
+
+            </div>
+            
+        </div>
+        </div>
+    )
+}
+
+export default InfoShimmer;
